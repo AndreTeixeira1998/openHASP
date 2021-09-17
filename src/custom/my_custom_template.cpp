a5310a0a6cef20dc0b7ac7c64ff47d3a685509d6
André_Teixeira_1998 <47570249+AndreTeixeira1998@users.noreply.github.com>
2021-09-17 11:57:03 +0100

    added example for message processing

--- src/custom/my_custom_template.cpp
+++ src/custom/my_custom_template.cpp
@@ -7,6 +7,60 @@
 
 #include "custom/my_custom.h"
 
+//custom publish message processing
+void publishmessageprocessing(const char* subtopic, const char* payload)
+{
+    //LOG_TRACE(99, F("%s => %s"), subtopic, payload);
+    if(!strcmp_P(subtopic, PSTR("idle"))){
+        if(!strcmp_P(payload, PSTR("short"))){
+            dispatch_text_line("backlight {state:1,brightness:122}", 99);
+        
+        } else if(!strcmp_P(payload, PSTR("long"))){
+            dispatch_text_line("backlight {state: 0}", 99);
+        
+        } 
+
+    } else if(!strcmp_P(subtopic, PSTR("p1b1"))){
+
+
+        if(strlen(payload) > 0) {
+
+            size_t maxsize = (128u * ((strlen(payload) / 128) + 1)) + 128;
+            DynamicJsonDocument json(maxsize);
+
+            // Note: Deserialization needs to be (const char *) so the objects WILL be copied
+            // this uses more memory but otherwise the mqtt receive buffer can get overwritten by the send buffer !!
+            DeserializationError jsonError = deserializeJson(json, payload);
+            json.shrinkToFit();
+
+            if(jsonError) { // Couldn't parse incoming JSON command
+                dispatch_json_error(99, jsonError);
+            } else {
+
+                const char* event_2;
+                bool val_2;
+
+
+                JsonVariant event      = json[F("event")];
+                JsonVariant val      = json[F("val")];
+
+                event_2 = event.as<const char*>();
+                val_2       = val.as<int>();
+
+                LOG_TRACE(99, F("%d ====> %s"), val_2, event_2);
+
+
+                }
+
+   
+        }
+    } //else if(subtopic == strstr_P(subtopic, PSTR("input"))) {}
+
+
+
+}
+
+
 void custom_setup()
 {
     // Initialization code here
